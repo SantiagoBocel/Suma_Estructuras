848b93855bd6886e47feb1ad06f33b95de7b63d6
Santiagobocel <santiagobocel10@gmail.com>
2019-08-23 22:00:24 -0600

    archivos

---
+++ Suma/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+        <h3>Santiago Bocel 1076818</h3>
+        <h1>Suma de numeros</h1>
+        <Suma/>
+    </div>
+</template>
+
+<script>
+    import Suma from './components/Suma.vue';
+    export default {
+        name: 'app',
+        components: {
+            Suma
+        }
+    }
+</script>
+<style>
+</style>
---
+++ Suma/src/components/Suma.vue
@@ -0,0 +1,34 @@
+<template>      
+     <div>
+         <input type="number" v-model="number.N1" />
+         <input type="number"  v-model="number.N2" />
+         <button type="button" v-on:click="Resultado(number.N1,number.N2)">
+           Contador        
+         </button>
+        
+     </div>               
+</template>
+<script>
+    export default{
+    data(){
+    return {
+        number: {
+            N1: ' ',
+            N2: ' ',
+        },
+        }
+       },    
+        methods: {
+            Resultado(num1, num2) {
+                var n1 = num1;
+                var n2 = num2;
+                var suma = parseInt(n1) + parseInt(n2); 
+                alert('Resultado' + ' ' + suma)
+            }
+       }
+    }
+</script>
+
+<style media=" screen ">
+
+</style>
