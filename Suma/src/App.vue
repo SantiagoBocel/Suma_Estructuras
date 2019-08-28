089ecc79d72a96f35f6187cc5e1787916b45b40d
Santiagobocel <santiagobocel10@gmail.com>
2019-08-27 20:41:05 -0600

    Operaciones de calculadora

--- Suma/src/App.vue
+++ Suma/src/App.vue
@@ -1,7 +1,8 @@
 <template>
     <div id="app">
         <h3>Santiago Bocel 1076818</h3>
-        <h1>Suma de numeros</h1>
+        <h4>Instrucciones: Cambiar el simbolo por la operacion que se desea realizar y luego precionar el boton </h4>
+        <h1>Operaciones de numeros</h1>
         <Suma/>
     </div>
 </template>
--- Suma/src/components/Suma.vue
+++ Suma/src/components/Suma.vue
@@ -1,8 +1,11 @@
-<template>      
+ <template>      
      <div>
-         <input type="number" v-model="number.N1" />
-         <input type="number"  v-model="number.N2" />
-         <button type="button" v-on:click="Resultado(number.N1,number.N2)">
+         <h4>Numero 1</h4>
+         <input type="number" v-model="number.N1" /><br /><h4>Simbolo</h4>
+         <input type="text" v-model="number.Simbolo" /><br />
+         <h4>Numero 2</h4>
+         <input type="number"  v-model="number.N2" /><br />
+         <button type="button" v-on:click="Resultado(number.N1,number.N2,number.Simbolo)">
            Contador        
          </button>
         
@@ -15,15 +18,31 @@
         number: {
             N1: ' ',
             N2: ' ',
+            Simbolo: '+',
         },
         }
        },    
         methods: {
-            Resultado(num1, num2) {
+         
+            Resultado(num1, num2, Simbolo) {
                 var n1 = num1;
                 var n2 = num2;
-                var suma = parseInt(n1) + parseInt(n2); 
+                var suma = parseInt(n1) + parseInt(n2);
+                var Resta = parseInt(n1) - parseInt(n2);
+                var Multiplicar = parseInt(n1) * parseInt(n2);
+                var Divicion = parseInt(n1) / parseInt(n2);
+                if (Simbolo == '+') {
                 alert('Resultado' + ' ' + suma)
+                }
+                if (Simbolo == '-') {
+                    alert('Resultado' + ' ' + Resta)
+                }
+                if (Simbolo == '*') {
+                    alert('Resultado' + ' ' + Multiplicar)
+                }
+                if (Simbolo == '/') {
+                    alert('Resultado' + ' ' + Divicion)
+                }
             }
        }
     }
